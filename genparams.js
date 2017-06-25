03a8249d5b75917abeaea7be2691f4d88ce26e65
Rui Carmo <rui.carmo@gmail.com>
2017-06-25 18:27:59 +0100

    schema

--- genparams.js
+++ genparams.js
@@ -4,7 +4,13 @@ const fs = require('fs'),
     SOLUTION_NAME = process.env.SOLUTION_NAME;
 
 fs.writeFileSync("parameters.json", JSON.stringify({
-    redisServerName: SOLUTION_NAME,
-    functionAppName: SOLUTION_NAME,
-    storagePrefix: SOLUTION_NAME.toLowerCase().replace(/\W+/g,'')
+    redisServerName: {
+        value: SOLUTION_NAME 
+    },
+    functionAppName: {
+        value: SOLUTION_NAME
+    },
+    storagePrefix: {
+        value: SOLUTION_NAME.toLowerCase().replace(/\W+/g,'')
+    }
 }))
